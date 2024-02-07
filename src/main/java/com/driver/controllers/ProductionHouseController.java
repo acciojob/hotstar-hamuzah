47554472320a588cfeeaa2460e94b8a8e86a7d8d
hamuzah <kiggunduhamuza16@gmail.com>
2024-02-07 13:19:49 +0300

    hotstar_hamzah

--- src/main/java/com/driver/controllers/ProductionHouseController.java
+++ src/main/java/com/driver/controllers/ProductionHouseController.java
@@ -22,7 +22,7 @@ public class ProductionHouseController {
     @PostMapping("/add")
     public Integer addProductionHouseInfoIntoDb(@RequestBody ProductionHouseEntryDto productionHouseEntryDto){
 
-        //Default ratings of the ProductionHouse should be 0
+        // Default ratings of the ProductionHouse should be 0
         return productionHouseService.addProductionHouseToDb(productionHouseEntryDto);
 
     }
